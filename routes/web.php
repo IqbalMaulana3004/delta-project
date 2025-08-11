a08cdfaf2a418ca8e1daef585dba6432ed0f5602
iqbalmaulana3004 <iqbal.maulanaa3004@gmail.com>
2025-08-12 05:01:45 +0800

    setup tailwind and fontawesome

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('delta-group');
+});
+
+Route::get('/delta-barito', function() {
+    return view('delta-barito');
+});
+
+Route::get('/delta-indo', function() {
+    return view('delta-indo');
+});
+
+Route::get('/delta-marine', function() {
+    return view('delta-marine');
 });
